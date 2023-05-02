a819eb56940042a6c5c8aa6eada443a17c980f68
RasheedSk <rasheek.s@icon-tg.com>
2023-05-02 19:09:34 +0530

    community chnage3

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -587,7 +587,7 @@ const Home = () => {
                                                 >
                                                     {e?.images?.map((element, i) => (
                                                         <Grid item sm={12} md={12} lg={12} key={i}>
-                                                            <Stack style={{padding:"5px" ,marginLeft:"-20px"}}>
+                                                            <Stack style={{padding:"10px" ,marginLeft:"-20px"}}>
                                                                 <div><img style={{ width: "380px", height: "240px" }} src={element} alt="" /></div>
                                                             </Stack>
                                                         </Grid>
